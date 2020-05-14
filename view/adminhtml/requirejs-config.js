4a85f380c40dc9f5604eeb76fae637833dfe93e4
Didi Kusnadi <dkusnadi@kemana.com>
2020-05-14 11:39:10 +0700

    fix js issue

--- view/adminhtml/requirejs-config.js
+++ view/adminhtml/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            'Magento_Sales/order/create/form':'Xtendable_UniqueTotal/order/create/form'
+            'Magento_Sales/order/create/form':'Xtendable_UniqueTotal/js/order/create/form'
         }
     }
 };
